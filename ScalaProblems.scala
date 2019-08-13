dfaf5597824edcf5fb20fc7220b4715512c78a07
leobouts <leobouts@gmail.com>
2019-08-14 01:00:39 +0300

    penultimate

--- ScalaProblems.scala
+++ ScalaProblems.scala
@@ -5,5 +5,13 @@ object ScalaProblems extends App{
     case _ :: tail => getLastElementFunctionally(tail)
     case _ => throw new NoSuchElementException
   }
-  
+
+  def getPenultimateFunctionally[A](ls: List[A]): A = ls match {
+    case h :: _ :: Nil => h
+    case _ :: tail => getPenultimateFunctionally(tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  println(getLastElementFunctionally(List(1,2,3,4,5)))
+  println(getPenultimateFunctionally(List(1,2,3,4,5)))
 }
